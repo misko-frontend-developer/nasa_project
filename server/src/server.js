b5e805c5be9ea2c4070ce68c07fff19c9d46497c
Milos <miskoo995@gmail.com>
2025-05-23 16:00:10 +0200

    adding tests

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,8 @@
 const http = require("http");
 
-const mongoose = require("mongoose");
 const app = require("./app");
-
+const { mongoConnect } = require("./services/mongo");
 const PORT = process.env.PORT || 8000;
-
-MONGO_URL =
-  "mongodb+srv://nasa-api:123456789!@cluster0.wfr1lzf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const server = http.createServer(app);
 
@@ -19,7 +15,7 @@ mongoose.connection.on("error", (error) => {
   console.error(error);
 });
 async function startServer() {
-  await mongoose.connect(MONGO_URL);
+  await mongoConnect();
   await loadPlanetsData();
 
   server.listen(PORT, () => {
